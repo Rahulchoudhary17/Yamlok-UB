52c57abdd175132cbfba1d963156376010f3e954
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2021-07-20 23:21:17 +0530

    Create __init__.py

---
+++ Yamlok/modules/sql_helper/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# the secret configuration specific things
+from var import Var
+
+
+def start() -> scoped_session:
+    engine = create_engine(Var.DB_URI, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
