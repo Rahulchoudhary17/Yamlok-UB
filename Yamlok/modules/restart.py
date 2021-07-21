900dc94947c12db5cb8b2a4561b4e51aad9c48e3
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2021-07-21 07:47:18 +0530

    Create restart.py

---
+++ Yamlok/modules/restart.py
@@ -0,0 +1,22 @@
+# (c) Copyright 2021-2022 Yamlok
+
+from telethon import events
+import os, re, sys, asyncio
+from Yamlok.utils import admin_cmd, sudo_cmd
+
+@bot.on(admin_cmd(pattern="restart"))
+@bot.on(sudo_cmd(pattern="restart ?(.*)", allow_sudo=True))
+async def _(event):
+    k = await bot.send_message(event.chat_id, '`Wait restarting`') 
+    asyncio.sleep(0.5)
+    await k.edit('`Restarted successfully`')
+    await event.delete()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit ()
+    
+# Made by AT-WORLDS-END
+
+@bot.on(admin_cmd(pattern="update")) 
+@bot.on(sudo_cmd(pattern="update ? (.*)", allow_sudo=True)) 
+async def update(event):
+     await event.edit("•• **ᴅᴏ ʏᴏᴜ ᴡᴀɴɴᴀ ᴜᴘᴅᴀᴛᴇ ʏᴏᴜʀ ᴅᴀɪsʏx ᴜsᴇʀʙᴏᴛ ᴛʜᴇɴ ᴊᴜsᴛ `.restart` ᴏɴʟʏ **••") 
