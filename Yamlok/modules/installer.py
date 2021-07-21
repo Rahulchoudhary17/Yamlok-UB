f53a036beda9a5cea5a67f663f514596629eb08a
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2021-07-21 18:52:30 +0530

    Create installer.py

---
+++ Yamlok/modules/installer.py
@@ -0,0 +1,57 @@
+# maded by shivam
+from pathlib import Path
+
+from Yamlok import CMD_LIST
+from Yamlok.utils import load_module, register
+
+DELETE_TIMEOUT = 5
+import os
+
+
+@register(pattern="^!install", outgoing=True)
+async def install(event):
+    a = "Installing."
+    b = 1
+    await event.edit(a)
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        try:
+            downloaded_file_name = (
+                await event.client.download_media(  # pylint:disable=E0602
+                    await event.get_reply_message(),
+                    "./Yamlok/modules/",  # pylint:disable=E0602
+                )
+            )
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                if shortname in CMD_LIST:
+                    string = "Commands found in {}\n".format(
+                        (os.path.basename(downloaded_file_name))
+                    )
+                    for i in CMD_LIST[shortname]:
+                        string += "  •  " + i
+                        string += "\n"
+                        if b == 1:
+                            a = "Installing.."
+                            b = 2
+                        else:
+                            a = "Installing..."
+                            b = 1
+                        await event.edit(a)
+                    return await event.edit(
+                        f"Installed module\n{shortname}\n\n{string}"
+                    )
+                return await event.edit(
+                    f"Installed module {os.path.basename(downloaded_file_name)}"
+                )
+            else:
+                os.remove(downloaded_file_name)
+                return await event.edit(
+                    f"Failed to Install \nError\nModule already installed or unknown formet"
+                )
+        except Exception as e:
+            await event.edit(f"Failed to Install \nError\n{str(e)}")
+            return os.remove(downloaded_file_name)
