1cc60f51fc2c69862c79b175abd216c54c91f38c
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2021-07-21 10:18:53 +0530

    Update _help.py

--- Yamlok/modules/_help.py
+++ Yamlok/modules/_help.py
@@ -45,18 +45,18 @@ async def cmd_list(event):
                 if input_str in CMD_HELP:
                     for i in CMD_HELP[input_str]:
                         string += i
-                    string += "\n\n**© @DaisyXOT**"
+                    string += "\n\n**© @YamlokOfficial**"
                     await event.edit(string)
                 else:
                     for i in CMD_LIST[input_str]:
                         string += "    " + i
                         string += "\n"
-                    string += "\n**© @DaisyXOT**"
+                    string += "\n**© @YamlokOfficial**"
                     await event.edit(string)
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[DaisyX Support](t.me/DaisySupport_Official)**\n\n"""
+            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[Yamlok Support](t.me/Yamloksupport)**\n\n"""
             try:
                 results = await bot.inline_query(  # pylint:disable=E0602
                     tgbotusername, help_string
@@ -67,5 +67,5 @@ async def cmd_list(event):
                 await event.delete()
             except BaseException:
                 await event.edit(
-                    f"This bot has inline disabled. Please enable it to use `{CMD_HNDLR}help`.\nGet help from [here](t.me/DaisySupport_Official)"
+                    f"This bot has inline disabled. Please enable it to use `{CMD_HNDLR}help`.\nGet help from [here](t.me/Yamloksupport)"
                 )
