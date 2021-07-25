a752ad42b347f7e674bb93b984411923364c2aa4
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-25 10:45:50 +0530

    Update sudo.py

--- Yamlok/modules/sudo.py
+++ Yamlok/modules/sudo.py
@@ -30,3 +30,28 @@ async def sudo(event):
      sender = await event.get_input_sender()
      sudolelo = os.environ.get("SUDO_USERS", None) 
      await edit_or_reply(event.chat_id, f"** sᴜᴅᴏʟɪsᴛ ᴏғ ** bot.me.username\n {sudolelo}")
+
+@bot.on(admin_cmd(pattern="addsudo"))
+async def tb(event):
+    ok = await eor(event, "α∂∂ιηg υsεя αs sυ∂σ υsεя...")
+    Lion = "SUDO_USERS"
+    if Var.HEROKU_APP_NAME is not None:
+        app = Heroku.app(Var.HEROKU_APP_NAME)
+    else:
+        await ok.edit("`[HEROKU]:" "\nPlease setup your` **HEROKU_APP_NAME**")
+        return
+    heroku_var = app.config()
+    if event is None:
+        return
+    try:
+        target = await get_user(event)
+    except Exception:
+        await ok.edit(f"Reply to a user.")
+    if sudousers:
+        newsudo = f"{sudousers} {target}"
+    else:
+        newsudo = f"{target}"
+    await ok.edit(
+        f"Aᴅᴅᴇᴅ `{target}` ᴀs ᴀ sᴜᴅᴏ ᴜsᴇʀ. ɴᴏᴡ ʀᴇsᴛᴀʀᴛɪɴɢ ʟɪᴏɴ X.. ɢɪᴍᴍᴇ sᴏᴍᴇ ᴍɪɴᴜᴛᴇs..."
+    )
+    heroku_var[Lion] = newsudo
